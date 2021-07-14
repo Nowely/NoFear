0900c048c3dfa5780078c2b31bce963670b7ffb2
Ruslan <ilyin-r@mail.ru>
2021-07-14 21:17:34 +0300

    Remove reportWebVitals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -12,8 +11,3 @@ ReactDOM.render(
     <App />
   </BrowserRouter>,
   rootElement);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
